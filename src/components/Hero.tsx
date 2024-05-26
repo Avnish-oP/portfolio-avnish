fa48e49722401e690be1b87086e4f002d4882259
Avnish-oP <kavnish1245@gmail.com>
2024-05-26 16:55:57 +0530

    hero section updates

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,6 +3,7 @@ import { TypewriterEffect } from "./ui/typewriter-effect";
 import { cn } from "@/lib/utils";
 import DotPattern from "./magicui/dot-pattern";
 import SocialMedia from "./socialMedia";
+import Link from "next/link";
 
 function Hero() {
   const words = [
@@ -26,43 +27,71 @@ function Hero() {
   ];
   return (
     <div className="relative flex h-full dark:bg-black w-full items-center justify-center overflow-hidden bg-background">
-      <div className="sm:flex lg:  min-h-[50vh] sm:min-h-screen w-full">
-        <div className="left mb-16 mt-10 basis-[75vw] flex flex-col justify-center items-center gap-6">
-          <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold antialiased">
+      <div className="sm:flex md:flex lg:  min-h-[70vh] lg:min-h-screen w-full justify-center items-center">
+        <div className="left mb-16 mt-20 basis-[75vw] flex flex-col justify-center items-center gap-10 lg:gap-6">
+          <h1 className="text-4xl mt-12 lg:mt-0 md:text-5xl lg:text-6xl font-bold antialiased">
             Hello,I{"'"}m Avnish
           </h1>
           <TypewriterEffect words={words} />
-          <button className="mt-4 bg-blue-500 text-white px-4 py-2 rounded-lg">
+          <Link href={"/contact"} className="mt-8 bg-blue-500 text-white px-4 py-2 rounded-lg">
             Contact Me
-          </button>
+          </Link>
         </div>
-        <SocialMedia />
+        <div className="right lg:mt-22">
+          <SocialMedia />
+        </div>
+      </div>
+      <div className=" lg:hidden opacity-80">
+        <DotPattern
+          width={20}
+          height={20}
+          cx={1}
+          cy={1}
+          cr={1}
+          x={0}
+          y={20}
+          className={cn(
+            "[mask-image:radial-gradient(300px_circle_at_center,white,transparent)]"
+          )}
+        />
+        <DotPattern
+          width={20}
+          height={20}
+          cx={1}
+          cy={1}
+          cr={1}
+          x={0}
+          y={20}
+          className={cn(
+            "[mask-image:radial-gradient(220px_circle_at_center,white,transparent)]"
+          )}
+        />
       </div>
       <div className="hidden lg:block">
-      <DotPattern
-        width={10}
-        height={10}
-        cx={1}
-        cy={1}
-        cr={1}
-        x={0}
-        y={20}
-        className={cn(
-          "[mask-image:radial-gradient(450px_circle_at_left,white,transparent)]"
-        )}
-      />
-      <DotPattern
-        width={10}
-        height={10}
-        cx={1}
-        cy={1}
-        cr={1}
-        x={0}
-        y={20}
-        className={cn(
-          "[mask-image:radial-gradient(450px_circle_at_right,white,transparent)]"
-        )}
-      />
+        <DotPattern
+          width={10}
+          height={10}
+          cx={1}
+          cy={1}
+          cr={1}
+          x={0}
+          y={20}
+          className={cn(
+            "[mask-image:radial-gradient(450px_circle_at_left,white,transparent)]"
+          )}
+        />
+        <DotPattern
+          width={10}
+          height={10}
+          cx={1}
+          cy={1}
+          cr={1}
+          x={0}
+          y={20}
+          className={cn(
+            "[mask-image:radial-gradient(450px_circle_at_right,white,transparent)]"
+          )}
+        />
       </div>
     </div>
   );
