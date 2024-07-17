0e21384b0dcba73fbd15eb1d8db12e9f2cb09c10
Avnish-oP <kavnish1245@gmail.com>
2024-07-17 08:40:08 +0530

    more responsive design

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -14,11 +14,10 @@ import {
   faGit,
   faGithubSquare,
   faGithub,
-  faFigma,
 } from "@fortawesome/free-brands-svg-icons";
 import { TextGenerateEffect } from "./ui/text-generate-effect";
 import Link from "next/link";
-import { BufferGeometry, Material, Mesh, NormalBufferAttributes, Object3DEventMap, Vector3 } from "three";
+import { BufferGeometry, Material, Mesh, Vector3 } from "three";
 
 function AboutMe() {
   const words =
@@ -44,26 +43,26 @@ function AboutMe() {
   ];
 
   const FloatingIcon = ({ icon, color }: { icon: any; color: string }) => {
-    const ref = useRef<Mesh<BufferGeometry<NormalBufferAttributes>, Material | Material[], Object3DEventMap>>(null);
+    const ref = useRef<Mesh<BufferGeometry, Material | Material[]>>(null);
     const [position, setPosition] = useState<Vector3>(new Vector3(...randomPosition()));
 
     useFrame(() => {
       if (ref.current) {
-        const currentRef: any = ref.current;
-        currentRef.rotation.y += 0.01;
-        currentRef.rotation.x += 0.01;
-        currentRef.position.y += 0.01;
-    
-        if (currentRef.position.y > 5) {
-          setPosition(new Vector3(...randomPosition()));
-          currentRef.position.set(...position);
+        ref.current.rotation.y += 0.01;
+        ref.current.rotation.x += 0.01;
+        ref.current.position.y += 0.01;
+
+        if (ref.current.position.y > 5) {
+          const newPosition = new Vector3(...randomPosition());
+          setPosition(newPosition);
+          ref.current.position.set(newPosition.x, newPosition.y, newPosition.z);
         }
       }
     });
 
     return (
       <mesh ref={ref} position={position}>
-        <Html center className="text-6xl" style={{ color }}>
+        <Html center className="text-3xl lg:text-6xl" style={{ color }}>
           <FontAwesomeIcon icon={icon} />
         </Html>
       </mesh>
@@ -73,15 +72,15 @@ function AboutMe() {
   return (
     <div className="flex flex-col md:flex-row h-screen dark:text-white justify-around items-center">
       <div className="md:w-1/2 flex flex-col justify-center items-center p-8">
-        <div className="text-center ">
+        <div className="text-center md:text-left">
           <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold antialiased mb-6">
             About Me
           </h1>
           <div className="text-lg md:text-xl lg:text-2xl mb-6">
             <TextGenerateEffect className="font-medium" words={words} />
           </div>
-          <Link href="/contact">
-            <button className="mx-auto px-6 py-2 bg-blue-600 text-white rounded-lg font-medium hover:font-bold flex items-center transform hover:-translate-y-1 transition duration-400">
+          <Link href="/contact" className="mx-auto px-6 py-2 bg-blue-600 text-white rounded-lg font-medium hover:font-bold flex items-center justify-center w-full transform hover:-translate-y-1 transition duration-400">
+             
               Contact Me
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -97,21 +96,17 @@ function AboutMe() {
                   d="M17 8l4 4m0 0l-4 4m4-4H3"
                 />
               </svg>
-            </button>
+            
           </Link>
         </div>
       </div>
-      <div className=" relative overflow-hidden lg:h-[70vh] h-full lg:w-[35vw] w-[85vw]">
+      <div className="relative overflow-hidden lg:h-[70vh] h-full lg:w-[35vw] w-[85vw]">
         <Canvas className="h-full">
           <Suspense fallback={null}>
             <Stars />
             <OrbitControls enableZoom={false} autoRotate />
             {icons.map((iconData, index) => (
-              <FloatingIcon
-                key={index}
-                icon={iconData.icon}
-                color={iconData.color}
-              />
+              <FloatingIcon key={index} icon={iconData.icon} color={iconData.color} />
             ))}
           </Suspense>
         </Canvas>
